fd3e041af3de6b88e3d7e202128206b3eda4ac17
theunblocker <102703115+theunblocker@users.noreply.github.com>
2022-04-23 12:38:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function darkmode() {
    var element = document.body;
    element.classList.toggle('darkmode');
 }
+
+function surprise(cb) { // Run surprise(function) and it'll run function() at april 29th, 12:10 or greater. I didn't even know that this was a feature in JS. (taken from stackoverflow)
+    (function loop() {
+        var now = new Date();
+        if (now.getDate() === 29 && now.getHours() === 12 && now.getMinutes() > 9) {
+            cb();
+        }
+        now = new Date();                  // allow for time passing
+        var delay = 60000 - (now % 60000); // exact ms to next minute interval
+        setTimeout(loop, delay);
+    })();
+}
